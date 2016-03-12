3d3784150818accf90302cc87dbb2f0b942d1d06
cedlemo <cedlemo@gmx.com>
2016-03-12 12:05:58 +0100

    Organize test env

--- tests/css_handler_tests.rb
+++ tests/css_handler_tests.rb
@@ -1,20 +1,44 @@
 #!/usr/bin/env ruby
 require "minitest/autorun"
 require "fileutils"
+require "sass"
 
 PATH = File.expand_path(File.dirname("__FILE__"))
 LIB_PATH = "#{PATH}/../lib"
 
 require "#{LIB_PATH}/css_handler"
+require "#{PATH}/test_files_ref"
 
 class TestCssHandlerModule < MiniTest::Test
+  include TestFilesRef
+
   def test_CssHandler_css_file?
-    assert(CssHandler.css_file?("#{PATH}/file1.css"))
+    assert(CssHandler.css_file?(SIMPLE_CSS))
   end
+  
   def test_CssHandler_scss_file?
-    assert(CssHandler.scss_file?("#{PATH}/file1.scss"))
+    assert(CssHandler.scss_file?(SIMPLE_SCSS))
   end
+  
   def test_CssHandler_sass_file?
-    assert(CssHandler.sass_file?("#{PATH}/file1.sass"))
+    assert(CssHandler.sass_file?(SIMPLE_SASS))
+  end
+  
+  def test_CssHandler_css_to_css
+    content = File.open(SIMPLE_CSS, "r").read
+    assert_equal(content, CssHandler.to_css(SIMPLE_CSS))
+
+    content = File.open(INHERIT_CSS, "r").read
+    assert_equal(content, CssHandler.to_css(INHERIT_CSS))
+  
+    content = File.open(COMMENTS_CSS, "r").read
+    assert_equal(content, CssHandler.to_css(COMMENTS_CSS))
+  end
+
+  def test_CssHandler_color_property?
+    assert_equal(true, CssHandler.color_property?("black"))
+    assert_equal(false, CssHandler.color_property?("toto"))
+    assert_equal(true, CssHandler.color_property?("rgb(0.5,0.6,0.4)"))
+    assert_equal(true, CssHandler.color_property?("rgba(0.5,0.6,0.4, 0.0)"))
   end
 end
---
+++ tests/test_files_ref.rb
@@ -0,0 +1,8 @@
+module TestFilesRef
+  SIMPLE_CSS = "#{PATH}/file1.css"
+  SIMPLE_SASS = "#{PATH}/file1.sass"
+  SIMPLE_SCSS = "#{PATH}/file1.scss"
+
+  INHERIT_CSS = "#{PATH}/file2.css"
+  COMMENTS_CSS = "#{PATH}/file3.css"
+end
