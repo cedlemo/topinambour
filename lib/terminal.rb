523d14941755543a7b55e6adce30718ac85e755d
cedlemo <cedlemo@gmx.com>
2016-03-23 18:18:17 +0100

    Add version 1.0.5 in panic mode

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -104,15 +104,15 @@ class TopinambourTerminal
   end
 
   def add_matches
-    @REGEXES = [:REGEX_URL_AS_IS, :REGEX_URL_FILE, :REGEX_URL_HTTP,
-                :REGEX_URL_VOIP, :REGEX_EMAIL, :REGEX_NEWS_MAN]
-    @REGEXES.each do |name|
-      regex_name = TopinambourRegex.const_get(name)
-      flags = [GLib::RegexCompileFlags::OPTIMIZE,
-               GLib::RegexCompileFlags::MULTILINE]
-      regex = GLib::Regex.new(regex_name, :compile_options => flags)
-      match_add_gregex(regex, 0)
-    end
+#    @REGEXES = [:REGEX_URL_AS_IS, :REGEX_URL_FILE, :REGEX_URL_HTTP,
+#                :REGEX_URL_VOIP, :REGEX_EMAIL, :REGEX_NEWS_MAN]
+#    @REGEXES.each do |name|
+#      regex_name = TopinambourRegex.const_get(name)
+#      flags = [GLib::RegexCompileFlags::OPTIMIZE,
+#               GLib::RegexCompileFlags::MULTILINE]
+#      regex = GLib::Regex.new(regex_name, :compile_options => flags)
+#      match_add_gregex(regex, 0)
+#    end
   end
 
   def display_copy_past_menu(widget, event)
