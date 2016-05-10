8c4439391e5849f92c9f971a0e2bd968144f0fba
cedlemo <cedlemo@gmx.com>
2016-05-10 12:41:00 +0200

    Add actions for combobox

--- lib/preferences.rb
+++ lib/preferences.rb
@@ -65,6 +65,9 @@ module TopinambourPreferences
       gen_switch_actions(prop_name, builder, parent)
     end
     gen_entry_actions("shell", builder, parent)
+    %w(cursor_shape cursor_blink_mode backspace_binding delete_binding).each do |prop_name|
+      gen_combobox_actions(prop_name, builder, parent)
+    end
   end
 
   def self.gen_switch_actions(property_name, builder, parent)
@@ -98,6 +101,14 @@ module TopinambourPreferences
         parent.shell = prop_value
         widget.text = prop_value
       end
+    end
+  end
+
+  def self.gen_combobox_actions(property_name, builder, parent)
+    combobox = builder["#{property_name}_sel"]
+    combobox.signal_connect "changed" do |widget|
+      value = widget.active_id.gsub(/_id/,"").to_sym
+      send_to_all_terminals(parent.notebook, "#{property_name}=", value)
     end
   end
 end
