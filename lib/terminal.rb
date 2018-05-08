6827e73a9ce5081d11ba9f3433a39a478af38744
cedlemo <cedlemo@gmx.com>
2018-05-08 12:09:01 +0200

    Terminal : add regexes support

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -35,6 +35,10 @@ end
 # The default vte terminal customized
 class TopinambourTerminal < Vte::Terminal
   attr_reader :pid, :menu
+  REGEXES = [:REGEX_URL_AS_IS, :REGEX_URL_FILE, :REGEX_URL_HTTP,
+                :REGEX_URL_VOIP, :REGEX_EMAIL, :REGEX_NEWS_MAN,
+                :CSS_COLORS]
+
   ##
   # Create a new TopinambourTerminal instance that runs command_string
   def initialize(command_string, working_dir = nil)
@@ -45,6 +49,7 @@ class TopinambourTerminal < Vte::Terminal
 
     signal_connect "child-exited" do |widget|
     end
+    add_matches
     set_size(180, 20)
   end
 
@@ -72,5 +77,74 @@ class TopinambourTerminal < Vte::Terminal
                  :spawn_flags => GLib::Spawn::SEARCH_PATH)
   rescue => e
     STDERR.puts e.message
+  end
+
+  def add_matches
+        REGEXES.each do |name|
+      regex_name = TopinambourRegex.const_get(name)
+      flags = [:optimize,
+               :multiline]
+      if Vte::Regex
+        # PCRE2_UTF | PCRE2_NO_UTF_CHECK | PCRE2_MULTILINE
+        pcre2_utf = "0x00080000".to_i(16)
+        pcre2_no_utf_check = "0x40000000".to_i(16)
+        pcre2_multiline = "0x00000400".to_i(16)
+        flags = pcre2_utf | pcre2_no_utf_check | pcre2_multiline
+        regex = Vte::Regex.new(regex_name, flags, :for_match => true)
+        match_add_regex(regex, 0)
+      else
+        regex = GLib::Regex.new(regex_name, :compile_options => flags)
+        match_add_gregex(regex, 0)
+      end
+    end
+  end
+
+  def display_copy_past_menu(widget, event)
+    x, y = event.window.coords_to_parent(event.x,
+                                         event.y)
+    rect = Gdk::Rectangle.new(x - allocation.x,
+                              y - allocation.y,
+                              1,
+                              1)
+    widget.menu.set_pointing_to(rect)
+    widget.menu.show
+  end
+
+  def manage_regex_on_right_click(_widget, event)
+    @last_match, _regex_type = match_check_event(event)
+  end
+
+  def manage_regex_on_click(_widget, event)
+    match, regex_type = match_check_event(event)
+    return nil if regex_type == -1
+    case REGEXES[regex_type]
+    when :REGEX_EMAIL
+      launch_default_for_regex_match("mailto:" + match, REGEXES[regex_type])
+    when :REGEX_URL_HTTP
+      launch_default_for_regex_match("http://" + match, @regexes[regex_type])
+    when :CSS_COLORS
+      launch_color_visualizer(match)
+    else
+      launch_default_for_regex_match(match, REGEXES[regex_type])
+    end
+  end
+
+  def launch_default_for_regex_match(match, regex_type)
+    Gio::AppInfo.launch_default_for_uri(match)
+  rescue => e
+    puts "error : #{e.message}\n\tfor match: #{match} of type :#{regex_type}"
+  end
+
+  def launch_color_visualizer(color_name)
+    dialog = Gtk::ColorChooserDialog.new(:title => color_name,
+                                         :parent => parent.toplevel)
+    dialog.show_editor = true
+    dialog.use_alpha = true
+    dialog.rgba = Gdk::RGBA.parse(color_name)
+    if dialog.run == Gtk::ResponseType::OK
+      clipboard = Gtk::Clipboard.get_default(Gdk::Display.default)
+      clipboard.text = dialog.rgba.to_s
+    end
+    dialog.destroy
   end
 end
