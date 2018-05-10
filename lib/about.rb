66a99ced289bcea94cc26e96b82ce82d1c095e2a
cedlemo <cedlemo@gmx.com>
2018-05-10 14:49:40 +0200

    About : add more information

--- lib/about.rb
+++ lib/about.rb
@@ -14,12 +14,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Topinambour.  If not, see <http://www.gnu.org/licenses/>.
 
+require "#{__dir__}/version"
 ## About stores metainformation about Topinambour.
 module About
   AUTHORS = ["Cedric Le Moigne <cedlemo@gmx.com>"].freeze
-  COMMENTS = "Terminal Emulator based on the ruby bindings of Gtk3 and Vte3".freeze
+  SUMMARY = "Vte3 Terminal emulator in ruby.".freeze
+  COMMENTS =
+    "Terminal Emulator based on the ruby bindings of Gtk3 and Vte3".freeze
   COPYRIGHT = "Copyright (C) 2015-2018 Cedric Le Moigne".freeze
-  LICENSE = "This program is licenced under the licence GPL-3.0 and later.".freeze
+  LICENSE = "GPL-3.0".freeze
+  LICENSE_COMMENTS =
+    "This program is licenced under the licence GPL-3.0 and later.".freeze
   LOGO_ICON_NAME = "utilities-terminal-symbolic".freeze
   PROGRAM_NAME = "Topinambour".freeze
   VERSION = Version::STRING
