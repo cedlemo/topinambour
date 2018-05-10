839d28e2e705515132b85694608623a946bd59da
cedlemo <cedlemo@gmx.com>
2018-05-10 14:25:35 +0200

    Store the version informations in a Version module

---
+++ lib/Version.rb
@@ -0,0 +1,25 @@
+# Copyright 2018 Cedric LE MOIGNE, cedlemo@gmx.com
+# This file is part of Topinambour.
+#
+# Topinambour is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Topinambour is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Topinambour.  If not, see <http://www.gnu.org/licenses/>.
+
+## Module that stores the version information for About class and
+#  the gemspec.
+module Version
+  MAJOR = 2
+  MINOR = 0
+  MICRO = 5
+
+  STRING = "#{MAJOR}.#{MINOR}.#{MICRO}".freeze
+end
