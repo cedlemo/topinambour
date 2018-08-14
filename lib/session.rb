04715f1e487ff675aa7382b2408f7c330fae8bb1
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-14 22:30:26 +0200

    Add copyright

--- lib/session.rb
+++ lib/session.rb
@@ -1,3 +1,22 @@
+# Copyright 2018 Cedric LE MOIGNE, cedlemo@gmx.com
+# This file is part of Topinambour.
+#
+# Topinambour is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Topinambour is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Topinambour.  If not, see <http://www.gnu.org/licenses/>.
+
+##
+# Session or Profile class
+# Allow to switch easily between different configurations.
 class Session
   attr_accessor :terminal_options,
                 :vte_options
